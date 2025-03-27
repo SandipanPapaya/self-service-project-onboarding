d11ee6c8caa1288e8d31a9378bc022e432254cbf
Sandipan Chakraborty <sandipanch@papayaglobal.com>
2025-03-27 15:57:17 +0530

    Initial commits

--- src/main/java/com/papaya/selfservice/SelfServiceApplication.java
+++ src/main/java/com/papaya/selfservice/SelfServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.selfservice;
+package com.papaya.selfservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
